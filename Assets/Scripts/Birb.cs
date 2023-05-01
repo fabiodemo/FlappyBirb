95a6da58348ba7de177b4fd68dbb966a0f1d3222
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-05-01 13:24:55 -0300

    Add files via upload

---
+++ Assets/Scripts/Birb.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birb : MonoBehaviour
+{
+    public float upForce = 1f;
+    private bool isDead = false;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D()
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirbDied();
+    }
+}
