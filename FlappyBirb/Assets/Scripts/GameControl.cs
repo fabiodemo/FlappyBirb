4435b5ec7c4a80ceb77a0c3567f7503ac2128828
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-05-01 13:27:11 -0300

    Inserted all missing files

---
+++ FlappyBirb/Assets/Scripts/GameControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public GameObject columnPrefab;
+    public Text scoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = 1f;
+    AudioSource audioSource;
+
+    private int score = 0;
+
+    void Awake()
+    {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+            return;
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void BirbDied()
+    {
+        audioSource.Play();
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
